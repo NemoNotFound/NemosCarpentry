4c10eb7567ed56860502daa552d09c3638680f0b
NemoNotFound <02grimes-bard@icloud.com>
2024-02-22 14:17:31 +0100

    Add table parts and recipes

--- src/main/java/com/nemonotfound/nemoscarpenting/item/ModItems.java
+++ src/main/java/com/nemonotfound/nemoscarpenting/item/ModItems.java
@@ -29,10 +29,95 @@ public class ModItems {
             new FabricItemSettings()), ItemGroups.TOOLS, ModItemGroups.NEMOS_FURNITURE);
     public static final Item NETHERITE_SAW = registerItem("netherite_saw", new SawItem(ToolMaterials.NETHERITE,
             new FabricItemSettings()), ItemGroups.TOOLS, ModItemGroups.NEMOS_FURNITURE);
+    public static final Item ACACIA_CHAIR_SEAT = registerItem("acacia_chair_seat", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item BAMBOO_CHAIR_SEAT = registerItem("bamboo_chair_seat", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item BIRCH_CHAIR_SEAT = registerItem("birch_chair_seat", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item CHERRY_CHAIR_SEAT = registerItem("cherry_chair_seat", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item CRIMSON_CHAIR_SEAT = registerItem("crimson_chair_seat", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item DARK_OAK_CHAIR_SEAT = registerItem("dark_oak_chair_seat", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item JUNGLE_CHAIR_SEAT = registerItem("jungle_chair_seat", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item MANGROVE_CHAIR_SEAT = registerItem("mangrove_chair_seat", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
     public static final Item OAK_CHAIR_SEAT = registerItem("oak_chair_seat", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item SPRUCE_CHAIR_SEAT = registerItem("spruce_chair_seat", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item WARPED_CHAIR_SEAT = registerItem("warped_chair_seat", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item ACACIA_CHAIR_LEG = registerItem("acacia_chair_leg", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item BAMBOO_CHAIR_LEG = registerItem("bamboo_chair_leg", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item BIRCH_CHAIR_LEG = registerItem("birch_chair_leg", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item CHERRY_CHAIR_LEG = registerItem("cherry_chair_leg", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item CRIMSON_CHAIR_LEG = registerItem("crimson_chair_leg", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item DARK_OAK_CHAIR_LEG = registerItem("dark_oak_chair_leg", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item JUNGLE_CHAIR_LEG = registerItem("jungle_chair_leg", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item MANGROVE_CHAIR_LEG = registerItem("mangrove_chair_leg", new Item(new FabricItemSettings()),
             ModItemGroups.NEMOS_FURNITURE);
     public static final Item OAK_CHAIR_LEG = registerItem("oak_chair_leg", new Item(new FabricItemSettings()),
             ModItemGroups.NEMOS_FURNITURE);
+    public static final Item SPRUCE_CHAIR_LEG = registerItem("spruce_chair_leg", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item WARPED_CHAIR_LEG = registerItem("warped_chair_leg", new Item(new FabricItemSettings()),
+            ModItemGroups.NEMOS_FURNITURE);
+    public static final Item ACACIA_TABLETOP_THE_CLASSIC = registerItem("acacia_tabletop_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item BAMBOO_TABLETOP_THE_CLASSIC = registerItem("bamboo_tabletop_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item BIRCH_TABLETOP_THE_CLASSIC = registerItem("birch_tabletop_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item CHERRY_TABLETOP_THE_CLASSIC = registerItem("cherry_tabletop_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item CRIMSON_TABLETOP_THE_CLASSIC = registerItem("crimson_tabletop_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item DARK_OAK_TABLETOP_THE_CLASSIC = registerItem("dark_oak_tabletop_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item JUNGLE_TABLETOP_THE_CLASSIC = registerItem("jungle_tabletop_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item MANGROVE_TABLETOP_THE_CLASSIC = registerItem("mangrove_tabletop_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item OAK_TABLETOP_THE_CLASSIC = registerItem("oak_tabletop_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item SPRUCE_TABLETOP_THE_CLASSIC = registerItem("spruce_tabletop_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item WARPED_TABLETOP_THE_CLASSIC = registerItem("warped_tabletop_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+
+    public static final Item ACACIA_TABLE_LEG_THE_CLASSIC = registerItem("acacia_table_leg_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item BAMBOO_TABLE_LEG_THE_CLASSIC = registerItem("bamboo_table_leg_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item BIRCH_TABLE_LEG_THE_CLASSIC = registerItem("birch_table_leg_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item CHERRY_TABLE_LEG_THE_CLASSIC = registerItem("cherry_table_leg_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item CRIMSON_TABLE_LEG_THE_CLASSIC = registerItem("crimson_table_leg_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item DARK_OAK_TABLE_LEG_THE_CLASSIC = registerItem("dark_oak_table_leg_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item JUNGLE_TABLE_LEG_THE_CLASSIC = registerItem("jungle_table_leg_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item MANGROVE_TABLE_LEG_THE_CLASSIC = registerItem("mangrove_table_leg_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item OAK_TABLE_LEG_THE_CLASSIC = registerItem("oak_table_leg_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item SPRUCE_TABLE_LEG_THE_CLASSIC = registerItem("spruce_table_leg_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
+    public static final Item WARPED_TABLE_LEG_THE_CLASSIC = registerItem("warped_table_leg_the_classic",
+            new Item(new FabricItemSettings()), ModItemGroups.NEMOS_FURNITURE);
 
     private static Item registerItem(String path, Item item, RegistryKey<ItemGroup>... itemGroups) {
         Item registeredItem = Registry.register(Registries.ITEM, new Identifier(MOD_ID, path), item);
