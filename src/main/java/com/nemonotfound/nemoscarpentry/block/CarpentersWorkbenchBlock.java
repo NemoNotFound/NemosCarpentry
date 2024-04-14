4eed7b20a06f28b5b58b08a65d28aec6a38e0230
NemoNotFound <02grimes-bard@icloud.com>
2024-04-14 22:27:04 +0200

    Update 2.4.0

--- src/main/java/com/nemonotfound/nemoscarpentry/block/CarpentersWorkbenchBlock.java
+++ src/main/java/com/nemonotfound/nemoscarpentry/block/CarpentersWorkbenchBlock.java
@@ -16,10 +16,10 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
-public class CarpentryTableBlock extends BlockWithEntity implements BlockEntityProvider {
+public class CarpentersWorkbenchBlock extends BlockWithEntity implements BlockEntityProvider {
 
 
-    public CarpentryTableBlock(Settings settings) {
+    public CarpentersWorkbenchBlock(Settings settings) {
         super(settings);
     }
 
@@ -43,6 +43,7 @@ public class CarpentryTableBlock extends BlockWithEntity implements BlockEntityP
                 ItemScatterer.spawn(world, pos, (CarpentryTableBlockEntity) blockEntity);
                 world.updateComparators(pos, this);
             }
+
             super.onStateReplaced(state, world, pos, newState, moved);
         }
     }
