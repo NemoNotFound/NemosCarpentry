df0f77bf3b789f39dd841c1fad83636f5ccc68ad
NemoNotFound <02grimes-bard@icloud.com>
2024-06-19 20:24:25 +0200

    Update to 1.20.5

--- src/main/java/com/nemonotfound/nemoscarpentry/property/ModProperties.java
+++ src/main/java/com/nemonotfound/nemoscarpentry/property/ModProperties.java
@@ -1,9 +1,11 @@
 package com.nemonotfound.nemoscarpentry.property;
 
 import com.nemonotfound.nemoscarpentry.block.enums.BenchPart;
+import com.nemonotfound.nemoscarpentry.block.enums.ChairPart;
 import net.minecraft.state.property.EnumProperty;
 
 public class ModProperties {
 
     public static final EnumProperty<BenchPart> BENCH_BLOCK_PART = EnumProperty.of("part", BenchPart.class);
+    public static final EnumProperty<ChairPart> CHAIR_PART = EnumProperty.of("part", ChairPart.class);
 }
